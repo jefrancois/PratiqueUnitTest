0a7b124a012e0e24f6d9c6bfdee49b6e3ef6f888
jefrancois <jacquesedomf@gmail.com>
2020-07-15 15:30:32 -0400

    Ajout de la fonction division

--- src/main/java/app/com/Calculator.java
+++ src/main/java/app/com/Calculator.java
@@ -2,7 +2,7 @@ package app.com;
 
 public class Calculator {
 
-	// Ok  addition
+	// Ok  
 	public int add(int a, int b) {
 		
 		if (a > b)
@@ -25,6 +25,19 @@ public class Calculator {
 			}
 		    return a + b;
 		}
+		
+		
+		// Ok  mult
+				public int Div(int a, int b) {
+					
+					if (a > b)
+					{a = a + 0;}
+					else
+					{
+						b = b + 0;
+					}
+				    return a + b;
+				}
 	
 	
 
